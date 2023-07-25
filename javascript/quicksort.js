31486e8fd8427c8be2c1ba7a3cf5e2d747e27d01
matthew rendely <matthew.rendely@gmail.com>
2023-07-24 19:49:35 -0700

    Solved

--- javascript/quicksort.js
+++ javascript/quicksort.js
@@ -1,20 +1,59 @@
 function partition(array, low, high) {
-  // type your code in here
+  let pivotIndex = high;
+  for (let i= low; i < pivotIndex; i++){
+
+    let curr = array[i]
+    let pivot = array[pivotIndex];
+    let swap = array[pivotIndex-1];
+    
+    if (curr >= pivot ){
+      array[i] = swap;
+      array[pivotIndex] = curr;
+      array[pivotIndex -1] = pivot;
+      pivotIndex--;
+      i--;
+    }
+    
+  }
+  return pivotIndex
 }
 
 function quicksort(array, low = 0, high = array.length - 1) {
-  // type your code here
+  if (low >= high) return array
+
+  let pivotIndex = partition(array, low, high);
+  
+  console.log('low', low, 'index', pivotIndex, 'high', high)
+
+  quicksort(array, low, pivotIndex-1);
+  quicksort(array, pivotIndex+1, high);
+  return array
 }
 
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: [1, 2, 3, 4]");
-  console.log(quicksort([3, 2, 1, 4]));
+  console.log("Expecting: [2, 4, 5, 5]");
+  console.log(partition([5, 2, 5, 4], 0, 3));
 
   console.log("");
 
+  console.log("Expecting: [2, 5]");
+  console.log(partition([5, 2], 0, 1));
+ 
+  console.log("");
+ 
+  console.log("Expecting: [1, 2, 3, 4]"); 
+  console.log(quicksort([3, 2, 1, 4]));
+
+  console.log("");
+ 
   console.log("Expecting: [1, 2, 2, 3, 4]");
   console.log(quicksort([1, 2, 2, 3, 4]));
+
+  console.log("");
+
+  console.log("Expecting: [1, 1, 2, 2, 3, 3]");
+  console.log(quicksort([3, 2, 3, 1, 2, 1]));
 }
 
 module.exports = quicksort;
